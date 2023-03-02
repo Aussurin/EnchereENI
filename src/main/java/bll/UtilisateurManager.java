5311f16f89c13c688b9b9ea8d12a1444ad232058
Angy AUSSURIN <angy.aussurin2022@campus-eni.fr>
2023-03-02 18:32:23 +0100

    Ajout de la methode pour ajouter un utilisateur

--- src/main/java/bll/UtilisateurManager.java
+++ src/main/java/bll/UtilisateurManager.java
@@ -10,21 +10,59 @@ import dal.DAOFactory;
 
 public class UtilisateurManager {
 	
-	private UtilisateurDAO utilisateurDao;
+	private static UtilisateurDAO utilisateurDAO;
+	
+	public Utilisateur checkUtilisateur(Utilisateur user) throws BLLException{
+		BLLException err = new BLLException();
+		if (user.getPseudo().length() > 30 || user.getPseudo().length() == 0) {
+			err.ajouterErreur("Pseudo trop long ou vide");
+		}
+		if (user.getNom().length() > 30 || user.getNom().length() == 0) {
+			err.ajouterErreur("Nom trop long ou vide");
+		}
+		if (user.getPrenom().length() > 30 || user.getPrenom().length() == 0) {
+			err.ajouterErreur("Prenom trop long ou vide");
+		}
+		if (user.getEmail().length() > 50 || user.getEmail().length() == 0) {
+			err.ajouterErreur("Email trop long ou vide");
+		}
+		Integer tel = user.getTelephone();
+		if (tel.toString().length() > 15 || tel.toString().length() == 0 ) {
+			err.ajouterErreur("Téléphone trop long ou vide");
+		}
+		if (user.getRue().length() > 30 || user.getRue().length() == 0) {
+			err.ajouterErreur("Rue trop long ou vide");
+		}
+		Integer codepostal = user.getCodePostal();
+		if (codepostal.toString().length() > 10 || codepostal.toString().length() == 0) {
+			err.ajouterErreur("Code postal invalide");
+		}
+		if (user.getVille().length() > 50 || user.getVille().length() == 0) {
+			err.ajouterErreur("Ville invalide");
+		}
+		if (user.getMotDePasse().length() > 30 || user.getMotDePasse().length() == 0) {
+			err.ajouterErreur("Mot de passe trop long ou vide");
+		}
+		if(err.getMessage() == null) {
+			insertUtilisateur(user);
+			return user;
+		}
+		throw err;
+	}
 	
 	public UtilisateurManager() {
-		utilisateurDao = DAOFactory.getUtilisateurDAO();
+		utilisateurDAO = DAOFactory.getUtilisateurDAO();
 	}
 	
 	public List<Utilisateur> selectAll(){
 		
-		return  utilisateurDao.selectAll();
+		return  utilisateurDAO.selectAll();
 
 	}
 	
 	public Utilisateur selectById(int id) {
 		
-		return utilisateurDao.selectById(id);
+		return utilisateurDAO.selectById(id);
 		
 	}
 	
@@ -33,7 +71,7 @@ public class UtilisateurManager {
 			
 		}
 		
-		utilisateurDao.insert(utilisateur);
+		utilisateurDAO.insert(utilisateur);
 		
 	}
 	
@@ -46,7 +84,7 @@ public class UtilisateurManager {
 	}
 	
 	public void delete (int id) {
-		utilisateurDao.delete(id);
+		utilisateurDAO.delete(id);
 		
 	}
 	
