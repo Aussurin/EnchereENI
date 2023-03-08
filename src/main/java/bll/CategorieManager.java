e3badee437d1f2e3d64d55ceabf214e13ddec288
Angy AUSSURIN <angy.aussurin2022@campus-eni.fr>
2023-03-09 00:04:37 +0100

    format

--- src/main/java/bll/CategorieManager.java
+++ src/main/java/bll/CategorieManager.java
@@ -8,27 +8,27 @@ import dal.DALException;
 import dal.DAOFactory;
 
 public class CategorieManager {
-private CategorieDAO categorieDAO;
+	private CategorieDAO categorieDAO;
 
 	public CategorieManager(CategorieDAO categorieDAO) {
-	categorieDAO = DAOFactory.getCategorieDAO();
+		categorieDAO = DAOFactory.getCategorieDAO();
 	}
-	
-	public void addCategorie(String libelle)throws Exception {
+
+	public void addCategorie(String libelle) throws Exception {
 		BLLException err = new BLLException();
-		
+
 		if (libelle.length() > 30 || libelle.length() == 0) {
 			err.ajouterErreur("Libelle de la catégorie trop longue ou vide");
 		}
-		 if (!categorieDAO.selectByLibelle(libelle).getLibelle().isEmpty()) {
-	            err.ajouterErreur("Cette catégorie existe déjà");
-	    if(err.getMessage() == null) {
-			insert(libelle);
-	    }
-	    throw err;
-	    }
-		 }
-	
+		if (!categorieDAO.selectByLibelle(libelle).getLibelle().isEmpty()) {
+			err.ajouterErreur("Cette catégorie existe déjà");
+			if (err.getMessage() == null) {
+				insert(libelle);
+			}
+			throw err;
+		}
+	}
+
 	public void deleteCategorie(Categorie cat) {
 		if (cat != null) {
 			this.categorieDAO.deleteCategorie(cat);
@@ -36,21 +36,21 @@ private CategorieDAO categorieDAO;
 			System.out.println("Impossible de supprimer cette catégorie !");
 		}
 	}
-	
-	public Categorie insert(String libelle) {		
-		return categorieDAO.insert(libelle);		
-	}
-	
-	public Categorie select(int noCategorie) throws DALException {		
-		return categorieDAO.selectByNo(noCategorie);		
-	}
-	
-	public Categorie update(Categorie cat) {	
-		categorieDAO.updateCategorie(cat);
-		return cat;	
+
+	public Categorie insert(String libelle) {
+		return categorieDAO.insert(libelle);
 	}
 
-	 public List<Categorie> getAllCategories() throws DALException {
-	        return categorieDAO.selectAll();
-	    }
+	public Categorie select(int noCategorie) throws DALException {
+		return categorieDAO.selectByNo(noCategorie);
+	}
+
+	public Categorie update(Categorie cat) {
+		categorieDAO.updateCategorie(cat);
+		return cat;
+	}
+
+	public List<Categorie> getAllCategories() throws DALException {
+		return categorieDAO.selectAll();
+	}
 }
